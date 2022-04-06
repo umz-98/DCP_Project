9639fc27ea44cdf622c0386dd275048b9e41fc9a
Fahkra Khan <FKhan@Fahkras-MacBook-Air.local>
2022-04-06 19:52:15 +0100

    adds unittes

--- tests/test_scraper.py
+++ tests/test_scraper.py
@@ -1,7 +1,12 @@
 import unittest
 
 from scraper.scraper import Scraper
-
 class TestScraper(unittest.TestCase):
     def Test_init(self):
         pass
+    # def Test_accept_cookies(self):
+    #     pass
+
+
+if __name__ == '__main__':
+    unittest.main()    
