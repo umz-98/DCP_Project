d518c0cff4c01c529b3d6bd7a6b299939ec600c6
Fahkra Khan <FKhan@Fahkras-MacBook-Air.local>
2022-04-07 20:52:47 +0100

    adds unittest cookies

--- tests/test_scraper.py
+++ tests/test_scraper.py
@@ -1,11 +1,19 @@
 import unittest
+from selenium.webdriver.common.by import By
 
 from scraper.scraper import Scraper
-class TestScraper(unittest.TestCase):
-    def Test_init(self):
-        pass
-    # def Test_accept_cookies(self):
+class testScraper(unittest.TestCase):
+    def setUp(self):
+        self.bot = Scraper()
+        return super().setUp()
+    def test_accept_cookies(self):
+        self.bot.accept_cookies(xpath = '//button[@class="btn btn-level1 accept-all-cookies"]')
+        self.bot.driver.find_element(By.XPATH, '//a[@class="logo"]' )
+    # def tearDown(self) -> None:
+    #     return super().tearDown()    
+    # def test_init(self):
     #     pass
+
 
 
 if __name__ == '__main__':
